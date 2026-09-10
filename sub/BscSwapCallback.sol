f75a639daf82eb07b86d610f09b341cf37afb726
jopiemanduapessy <manduapessyjopie@gmail.com>
2026-09-11 03:54:11 +0700

    Create BscSwapCallback contract

---
+++ sub/BscSwapCallback.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title BscSwapCallback
+ * @dev Callback contract untuk BSC Swap operations
+ */
+contract BscSwapCallback {
+    // Callback implementation akan ditambahkan di sini
+}
