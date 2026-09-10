0cb7f8d098c17ee58183cc1908f0ecd8ac7e3cee
jopiemanduapessy <manduapessyjopie@gmail.com>
2026-09-11 03:54:32 +0700

    Create main Swap.sol contract

---
+++ Swap.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./interfaces/Symbol.sol";
+import "./interfaces/Pair.sol";
+import "./interfaces/Pool.sol";
+import "./interfaces/IStaticCall.sol";
+import "./interfaces/ISingleton.sol";
+import "./sub/BscSwapCallback.sol";
+
+/**
+ * @title Swap
+ * @dev Contract utama untuk swap operations di BSC
+ */
+contract Swap {
+    // Implementation akan ditambahkan di sini
+}
