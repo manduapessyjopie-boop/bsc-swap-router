0b79db4dbd7fca9a2e5e93d2815e9094b86c23c9
jopiemanduapessy <manduapessyjopie@gmail.com>
2026-09-11 03:55:28 +0700

    Create ISingleton interface

---
+++ interfaces/ISingleton.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title ISingleton
+ * @dev Interface untuk Singleton pattern
+ */
+interface ISingleton {
+    // Singleton interface functions akan ditambahkan di sini
+}
