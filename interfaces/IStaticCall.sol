d7cbf09c474dff2a579ce403665352dddf788f3f
jopiemanduapessy <manduapessyjopie@gmail.com>
2026-09-11 03:55:22 +0700

    Create IStaticCall interface

---
+++ interfaces/IStaticCall.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title IStaticCall
+ * @dev Interface untuk Static Call operations
+ */
+interface IStaticCall {
+    // Static Call interface functions akan ditambahkan di sini
+}
