c9d7e65ed76aa8dc76fc0f46227c2a3cab13a586
jopiemanduapessy <manduapessyjopie@gmail.com>
2026-09-11 03:54:49 +0700

    Create Symbol interface

---
+++ interfaces/Symbol.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title ISymbol
+ * @dev Interface untuk Symbol
+ */
+interface ISymbol {
+    // Symbol interface functions akan ditambahkan di sini
+}
