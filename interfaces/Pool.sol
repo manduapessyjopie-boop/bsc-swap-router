b5768edd16215db88721d1690a55700d1d0568a7
jopiemanduapessy <manduapessyjopie@gmail.com>
2026-09-11 03:55:10 +0700

    Create Pool interface

---
+++ interfaces/Pool.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title IPool
+ * @dev Interface untuk Pool
+ */
+interface IPool {
+    // Pool interface functions akan ditambahkan di sini
+}
