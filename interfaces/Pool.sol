8dfe8a6b7136ab026282f6ba6cdb942f58c11f5f
jopiemanduapessy <manduapessyjopie@gmail.com>
2026-09-11 04:02:35 +0700

    Update Pool.sol

--- interfaces/Pool.sol
+++ interfaces/Pool.sol
@@ -1,10 +1,20 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
 
-/**
- * @title IPool
- * @dev Interface untuk Pool
- */
-interface IPool {
-    // Pool interface functions akan ditambahkan di sini
+interface Pool {
+       function swap(address recipient,
+                             bool zeroForOne,
+                             int256 amountSpecified,
+                             uint160 sqrtPriceLimitX96,
+                            bytes calldata data) 
+                            external  returns (int256 amount0, int256 amount1) ;
+           function slot0() external view returns (
+                            uint160 sqrtPriceX96,
+                            int24 tick,
+                            uint16 observationIndex,
+                            uint16 observationCardinality,
+                            uint16 observationCardinalityNext,
+                            uint32 feeProtocol,
+                            bool unlocked
+                            );
 }
