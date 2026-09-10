e29b709f8ee727e74b8cb06d37c6908056e8fee7
jopiemanduapessy <manduapessyjopie@gmail.com>
2026-09-11 03:55:01 +0700

    Create Pair interface

---
+++ interfaces/Pair.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title IPair
+ * @dev Interface untuk Pair
+ */
+interface IPair {
+    // Pair interface functions akan ditambahkan di sini
+}
